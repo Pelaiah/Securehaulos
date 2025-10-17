ae53d86a8149c4a5bea58c130db4a73635d2088d
Tadiwanashe Ngarande (Mufaro foundation) <mufarofoundation@gmail.com>
2025-10-17 21:05:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6798971086-4a543",
+  "appId": "1:250899565347:web:40e59f6f0f2d6c3b86da96",
+  "apiKey": "AIzaSyBoEEfzzyrW3yT9CT6rP4gTGK-Nr_40YGg",
+  "authDomain": "studio-6798971086-4a543.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "250899565347"
+};
