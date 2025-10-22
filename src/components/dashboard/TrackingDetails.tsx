3234a5cae491b3ebc5708a131b9060ef27074b50
Tadiwanashe Ngarande (Mufaro foundation) <mufarofoundation@gmail.com>
2025-10-22 10:51:38 +0000

    reduce the map height by 40%

--- src/components/dashboard/TrackingDetails.tsx
+++ src/components/dashboard/TrackingDetails.tsx
@@ -91,7 +91,7 @@ export function TrackingDetails({ truck }: TrackingDetailsProps) {
                             </div>
                         </CardHeader>
                         <CardContent>
-                            <div className="h-64 rounded-lg overflow-hidden">
+                            <div className="h-40 rounded-lg overflow-hidden">
                                 <Map trucks={[truck]} selectedTruckId={truck.id} />
                             </div>
                         </CardContent>
