4a822f247da4cdd2c3ca299f4c928c33f9e40a44
Tadiwanashe Ngarande (Mufaro foundation) <mufarofoundation@gmail.com>
2025-11-04 11:23:02 +0000

    eplace with create account (_for element <Button>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,7 +75,7 @@ export default function Home() {
               <div className="flex flex-col sm:flex-row items-start justify-start gap-4">
                 <SignUpModal>
                   <Button size="lg" className="w-full sm:w-auto">
-                    Get Your First Load <ArrowRight className="ml-2 h-5 w-5" />
+                    Create Account <ArrowRight className="ml-2 h-5 w-5" />
                   </Button>
                 </SignUpModal>
               </div>
