7c03273a4ec76008ebc89c7a1df59702ad761a73
Tadiwanashe Ngarande (Mufaro foundation) <mufarofoundation@gmail.com>
2025-11-06 07:55:51 +0000

    this is where i want the truck to be the background image should be move

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
             alt={heroImage.description}
             fill
             className="object-cover object-center opacity-30"
-            style={{ objectPosition: '65% 50%' }}
+            style={{ objectPosition: '80% 50%' }}
             data-ai-hint={heroImage.imageHint}
             priority
             />
